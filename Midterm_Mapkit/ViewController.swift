454a54254b711a0ab0d8806aa45b16774e61c158
Austin Herman <ashe223@g.uky.edu>
2019-09-24 13:30:49 -0400

    inital commit -- some tutorial set up complete not all

---
+++ Midterm_Mapkit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midterm_Mapkit
+//
+//  Created by  on 9/24/19.
+//  Copyright © 2019 Team Android. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
