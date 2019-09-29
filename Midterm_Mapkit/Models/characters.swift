51b017d15377c307b3dc8e9121072dc4f865ee62
Austin Herman <ashe223@g.uky.edu>
2019-09-29 14:15:52 -0400

    tutorial finished

---
+++ Midterm_Mapkit/Models/characters.swift
@@ -0,0 +1,45 @@
+//
+//  characters.swift
+//  Midterm_Mapkit
+//
+//  Created by  on 9/29/19.
+//  Copyright © 2019 Team Android. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+// character class defines name and color as optional properties.
+class Character: MKCircle
+{
+    var name: String?
+    var color: UIColor?
+    
+    // convenience initializer accepts a plist filename and color to draw the circle
+    convenience init(filename: String, color: UIColor)
+    {
+        // Then it reads in the data from the plist file
+        guard let points = Park.plist(filename) as? [String] else
+        {
+            self.init()
+            return
+        }
+        // selects a random location from the four locations in the file
+        let cgPoints = points.map {
+            CGPointFromString($0)
+        }
+        
+        let coords = cgPoints.map {
+            CLLocationCoordinate2DMake(CLLocationDegrees($0.x), CLLocationDegrees($0.y))
+        }
+        
+        //chooses a random radius to simulate time variance
+        let randomCenter = coords[Int(arc4random()%4)]
+        let randomRadius = CLLocationDistance(max(5, Int(arc4random()%40)))
+    
+        self.init(center: randomCenter, radius: randomRadius)
+        
+        self.name = filename
+        self.color = color
+  }
+}
--- Midterm_Mapkit/View Controllers/ParkMapViewController.swift
+++ Midterm_Mapkit/View Controllers/ParkMapViewController.swift
@@ -45,6 +45,15 @@ class ParkMapViewController: UIViewController {
                 case .mapPins:
                     addAttractionPins()
                 
+                case .mapRoute:
+                    addRoute()
+                
+                case .mapBoundary:
+                    addBoundary()
+                
+                case .mapCharacterLocation:
+                addCharacterLocation()
+                
                 default:
                     break;
             }
@@ -94,27 +103,96 @@ class ParkMapViewController: UIViewController {
       }
     }
 
+    // reads EntranceToGoliathRoute plist, and converts the coordinates to CLLocationCoordinate2D objecys.
+    func addRoute()
+    {
+        guard let goliathpoints = Park.plist("EntranceToGoliathRoute") as? [String]
+        else {
+            print("addroute::did not read coords")
+            return
+        }
+        
+        let cgPoints = goliathpoints.map {
+            CGPointFromString($0)
+        }
+        
+        let coords = cgPoints.map {
+            CLLocationCoordinate2DMake(CLLocationDegrees($0.x), CLLocationDegrees($0.y))
+        }
+        
+        let myPolyline = MKPolyline(coordinates: coords, count: coords.count)
+        
+        print("addroute:: adding polyline: ", myPolyline, " of type :", type(of: myPolyline))
+        
+        mapView.add(myPolyline)
+    }
+    
+    // given the boundary array and point count from the park instance, create a new MKPolygon
+    func addBoundary() {
+        mapView.add(MKPolygon(coordinates: park.boundary, count: park.boundary.count))
+    }
+    
+    // passes the plist filename for each ride and a color, then it to the map as an overlay.
+    func addCharacterLocation() {
+      mapView.add(Character(filename: "BatmanLocations", color: .blue))
+      mapView.add(Character(filename: "TazLocations", color: .orange))
+      mapView.add(Character(filename: "TweetyBirdLocations", color: .yellow))
+    }
 }
 
 extension ParkMapViewController: MKMapViewDelegate
 {
     func mapView(_ mapView : MKMapView, rendererFor overlay : MKOverlay) -> MKOverlayRenderer
     {
-      if overlay is ParkMapOverlay
-      {
-        return ParkMapOverlayView(overlay : overlay, overlayImage : #imageLiteral(resourceName: "overlay_park"))
-      }
-      
-      return MKOverlayRenderer()
+        print("mapView::overlay renderer:: called")
+        if overlay is ParkMapOverlay
+        {
+            print("mapView::overlay renderer:: returning park map overlay view")
+            return ParkMapOverlayView(overlay: overlay, overlayImage: #imageLiteral(resourceName: "overlay_park"))
+        }
+        else if overlay is MKPolyline // look for mkpolyline objects
+        {
+            let lineview = MKPolylineRenderer(overlay: overlay)
+            lineview.strokeColor = UIColor.green
+            
+            print("mapView::overlay renderer:: returning line view: ", lineview)
+            
+            return lineview
+        }
+        else if overlay is MKPolygon // create an MKOverlayView as a MKPolygonRenderer
+        {
+            let polygonView = MKPolygonRenderer(overlay: overlay)
+            polygonView.strokeColor = UIColor.magenta
+            
+            print("mapView::overlay renderer:: returning polygon view: ", polygonView)
+            
+            return polygonView
+        }
+        else if let character = overlay as? Character
+        {
+            let circleView = MKCircleRenderer(overlay: character)
+            circleView.strokeColor = character.color
+            
+            print("mapView::overlay renderer:: returning circle view: ", circleView)
+            
+            return circleView
+        }
+        
+        print("mapView::overlay renderer:: returning generic mk overlay renderer")
+        return MKOverlayRenderer()
     }
     
     func mapView(_ mapView : MKMapView, viewFor annotation : MKAnnotation) -> MKAnnotationView?
     {
-      let annotationView = AttractionAnnotationView(annotation: annotation, reuseIdentifier: "Attraction")
+        print("mapView::annotation view:: called")
         
-      annotationView.canShowCallout = true
+        let annotationView = AttractionAnnotationView(annotation: annotation, reuseIdentifier: "Attraction")
         
-      return annotationView
+        annotationView.canShowCallout = true
+        
+        print("mapView::annotation view:: returning", annotationView, " of type: ", type(of: annotationView))
+        
+        return annotationView
     }
 
 }
